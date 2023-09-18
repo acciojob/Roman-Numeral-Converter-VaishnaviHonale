489bc9be464c2b8fe2b5655faaaa02903322410e
VaishnaviHonale <137199492+VaishnaviHonale@users.noreply.github.com>
2023-09-18 15:39:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,6 +8,40 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
+	 var res = 0;
+ 
+     for (i = 0; i < num.length; i++)
+     {
+         // Getting value of symbol s[i]
+         var s1 = value(num.charAt(i));
+ 
+         // Getting value of symbol s[i+1]
+         if (i + 1 < num.length)
+         {
+             var s2 = value(num.charAt(i + 1));
+ 
+             // Comparing both values
+             if (s1 >= s2)
+             {
+                 // Value of current symbol
+                 // is greater or equalto
+                 // the next symbol
+                 res = res + s1;
+             }
+             else
+             {
+                 // Value of current symbol is
+                 // less than the next symbol
+                 res = res + s2 - s1;
+                 i++;
+             }
+         }
+         else 
+         {
+             res = res + s1;
+         }
+     }
+     return res;
 
   //your code here
 
